f612938444f8da8b7c49468947e8d3bcd1a40c2f
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-24 17:00:21 +0300

    upgrade

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,7 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
+import { AsyncPaginate } from "react-select-async-paginate"
 
-function search() {
-  return <div>Hello</div>
+function search({ onSearchChange }) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [search, setSearch] = useState(null)
+
+  const handleOnChange = searchData => {
+    setSearch(searchData)
+    onSearchChange(searchData)
+  }
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+    />
+  )
 }
 
 export default search
