55bd478098f7ed08a5dad01d7723f51c61636f51
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-25 09:45:18 +0300

    upgrade

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,7 +1,15 @@
 import "./current-weather.css"
 
 const CurrentWeather = () => {
-  return "hello"
+  return (
+    <div className="weather">
+      <div className="top">
+        <p className="city">Belgrade</p>
+        <p className="weather-description">Sunny</p>
+      </div>
+      <img src="icons/01d.png" alt="weather" className="weather-icon" />
+    </div>
+  )
 }
 
 export default CurrentWeather
