429c915da624965b5502842875956d95777eebbe
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-25 09:56:44 +0300

    Update current-weather.js

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -4,10 +4,12 @@ const CurrentWeather = () => {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city">Belgrade</p>
-        <p className="weather-description">Sunny</p>
+        <div>
+          <p className="city">Belgrade</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img src="icons/01d.png" alt="weather" className="weather-icon" />
       </div>
-      <img src="icons/01d.png" alt="weather" className="weather-icon" />
     </div>
   )
 }
