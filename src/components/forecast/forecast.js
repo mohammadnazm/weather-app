8bf5777418d13532a1eefe517a7f1c417974d0bb
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-25 20:23:58 +0300

    Upgrade

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,10 +1,24 @@
-import { Accordion } from "react-accessible-accordion"
+import {
+  Accordion,
+  AccordionItemPanel,
+  AccordionItemHeading,
+  AccordionItemButton,
+} from "react-accessible-accordion"
 
-const Forecast = () => {
+const Forecast = ({ data }) => {
   return (
     <>
       <label className="title">Daily</label>
-      <Accordion allowZeroExpanded></Accordion>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, idx) => (
+          <Accordion>
+            <AccordionItemHeading>
+              <AccordionItemButton>Hello</AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </Accordion>
+        ))}
+      </Accordion>
     </>
   )
 }
