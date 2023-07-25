71ff531ce5fdf2df8020ce9003fbfcc6b8ede31f
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-25 19:15:15 +0300

    Update current-weather.js

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -32,11 +32,11 @@ const CurrentWeather = ({ data }) => {
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
-            <span className="parameter-value">15%</span>
+            <span className="parameter-value">{data.main.humidity}</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
-            <span className="parameter-value">15 hPa</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
           </div>
         </div>
       </div>
