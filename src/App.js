99941e03c408afbe9f4cdf49c610fbaa1e84a255
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-25 16:17:20 +0300

    upgrade

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css"
 import Search from "./components/search/search"
 import CurrentWeather from "./components/current-weather/current-weather"
+import { WEATHER_API_KEY } from "./api"
 
 function App() {
   const handleOnSearchChange = searchData => {
-    console.log(searchData)
+    const [lat, lon] = searchData.value.split(" ")
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_KEY}/weather?lat={lat}&lon={lon}&appid={API key}`
+    )
   }
 
   return (
