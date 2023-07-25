11235aa080e4b2e53935ea9fb80669ad1cc2630c
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-25 17:34:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css"
 import Search from "./components/search/search"
 import CurrentWeather from "./components/current-weather/current-weather"
-import { WEATHER_API_KEY } from "./api"
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./api"
 
 function App() {
   const handleOnSearchChange = searchData => {
     const [lat, lon] = searchData.value.split(" ")
 
     const currentWeatherFetch = fetch(
-      `${WEATHER_API_KEY}/weather?lat={lat}&lon={lon}&appid={API key}`
+      `${WEATHER_API_KEY}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
     )
+
+    const forecastFetch = fetch(
+      `${WEATHER_API_KEY}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    )
+
+    Promise.all([currentWeatherFetch, forecastFetch]).then(async response => {
+      const weatherResponse = await response[0].json()
+      const forecastResponse = await response[1].json()
+    })
   }
 
   return (
