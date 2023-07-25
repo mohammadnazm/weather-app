0a92678ca84fab0c1444306d5797d7e53331615a
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-26 01:03:10 +0300

    add panel

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -51,6 +51,22 @@ const Forecast = ({ data }) => {
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
+            <AccordionItemPanel>
+              <div className="daily-details-grid">
+                <div className="daily-details-item">
+                  <label>Pressure</label>
+                  <label>{item.main.pressure}</label>
+                </div>
+                <div className="daily-details-item">
+                  <label>Humidity</label>
+                  <label>{item.main.humidity}</label>
+                </div>
+                <div className="daily-details-item">
+                  <label>Clouds</label>
+                  <label>{item.clouds.all}</label>
+                </div>
+              </div>
+            </AccordionItemPanel>
           </AccordionItem>
         ))}
       </Accordion>
