4e7d0db70f42e5ca2bd1967ed918e1ecac01704b
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-25 17:53:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css"
 import Search from "./components/search/search"
 import CurrentWeather from "./components/current-weather/current-weather"
-import { WEATHER_API_KEY, WEATHER_API_KEY } from "./api"
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api"
 import { useState } from "react"
 
 function App() {
