ae3ecb88c5d5181bf5063b40a4f7f6e01a48b5c1
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-25 17:43:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import "./App.css"
 import Search from "./components/search/search"
 import CurrentWeather from "./components/current-weather/current-weather"
 import { WEATHER_API_KEY, WEATHER_API_URL } from "./api"
+import { useState } from "react"
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState()
+  const [forecast, setForecast] = useState()
+
   const handleOnSearchChange = searchData => {
     const [lat, lon] = searchData.value.split(" ")
 
@@ -18,6 +22,9 @@ function App() {
     Promise.all([currentWeatherFetch, forecastFetch]).then(async response => {
       const weatherResponse = await response[0].json()
       const forecastResponse = await response[1].json()
+
+      setCurrentWeather({ city: searchData.label, ...weatherResponse })
+      setForecast({ city: searchData.label, ...forecastResponse })
     })
   }
 
